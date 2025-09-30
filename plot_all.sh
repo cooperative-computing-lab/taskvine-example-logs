4b30937a2d55c3eeb2fc87c8f6680c8f07ffc402
Douglas Thain <dthain@nd.edu>
2025-09-30 12:52:24 -0400

    Clean up output

--- plot_all.sh
+++ plot_all.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-for workflow in lnni
+for workflow in lnni examol
 do
+	mkdir -p ${workflow}/plots
+
 	echo "plotting ${workflow} performance log (usually fast)"
 	vine_graph_log -Tpng -o ${workflow}/plots/performance ${workflow}/vine-logs/performance
 
-	echo "plotting ${workflow} transaction log (usually fast)"
+	echo "plotting ${workflow} transaction log (usually a bit slower)"
 	vine_plot_txn_log --mode workers ${workflow}/vine-logs/transactions ${workflow}/plots/txn-workers.png
 
-	echo "plotting ${workflow} taskgraph (usually sloooww)"
-	dot -Tpng ${workflow}/vine-logs/taskgraph > ${workflow}/plots/taskgraph.png
+	#echo "plotting ${workflow} taskgraph (usually sloooww)"
+	#dot -Tpng ${workflow}/vine-logs/taskgraph > ${workflow}/plots/taskgraph.png
 done
 
